963ae6111b6b97ddd16c04413b02dab391170163
David Wang <davidew2@illinois.edu>
2016-09-15 20:47:30 -0500

    finished web looks bad, okay enjoy

--- public/js/angularjs/app.js
+++ public/js/angularjs/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('app', [ 'listHall']);
+var app = angular.module('app', []);
+
+app.controller('mainController', ['$scope', '$http', function($scope, $http)
+{
+  $scope.halls = [];
+  $http.get("http://laundryscrape-dev.us-east-1.elasticbeanstalk.com/halls").then(function(response)
+  {
+    console.log("test");
+    angular.copy(response.data, $scope.halls);
+    console.log(response);
+  });
+}]);
